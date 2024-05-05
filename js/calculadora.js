4e37fc947a2b24b10e1bf41f342206d1cb61205a
MMdelosSantos <magdalenadls@outlook.com>
2024-05-04 21:18:42 -0300

    Correciones en formato de presupuestos guardados

--- js/calculadora.js
+++ js/calculadora.js
@@ -45,11 +45,13 @@ function calculo() {   // Declaro variables principales
 
     if (alquilaLocal) {
         totalCosto += costoAlquiler
-        console.log(alquilaLocal)
     }
 
     // Mostrar los resultados obtenidos
-    document.getElementById('resultados').innerHTML = "Tienes una cantidad total de : " + cantidadInvitadosTotal + ' invitado/s, de los cuales ' + cantidadInvitadosVeg + ' es/son vegetariano/s <br> Del total de invitados, hay ' + cantidadGenMujer + ' que es/son mujer/es, ' + cantidadGenHombre + ' que es/son hombre/s y ' + cantidadGenOtros + ' es/son de otro género.<br> El costo total de tu evento, considerando estos invitados y el alquiler del local de $' + costoAlquiler + ' es de: $' + totalCosto
+    document.getElementById('resultados').innerHTML = "Tienes una cantidad total de : " + cantidadInvitadosTotal + ' invitado/s, de los cuales ' +
+        cantidadInvitadosVeg + ' es/son vegetariano/s <br> Del total de invitados, hay ' + cantidadGenMujer + ' que es/son mujer/es, ' + cantidadGenHombre +
+        ' que es/son hombre/s y ' + cantidadGenOtros + ' es/son de otro género.<br> El costo total de tu evento, considerando estos invitados y el alquiler del local de $' +
+        costoAlquiler + ' es de: $' + totalCosto
 
     let nuevaVersionPpto = new Presupuestos();
 
@@ -61,17 +63,17 @@ function calculo() {   // Declaro variables principales
     nuevaVersionPpto.cantidadGenOtros = `${cantidadGenOtros}`;
     nuevaVersionPpto.costoAlquiler = `${costoAlquiler}`;
     nuevaVersionPpto.totalCosto = `${totalCosto}`;
-    sessionStorage.setItem('VersionPresupuesto', JSON.stringify(nuevaVersionPpto))
 
-    return false;
+    let JsonNuevaVersion = JSON.stringify(nuevaVersionPpto)
+    sessionStorage.setItem('VersionPresupuesto', JsonNuevaVersion)
+
 }
 
 // Versiones de presupuestos guardados:
 
-
 class Presupuestos {
     static VersionPpto = 1;
-    constructor() {
+    constructor(numero, cantidadInvitadosTotal, cantidadInvitadosVeg, cantidadInvitadosNoVeg, cantidadGenMujer, cantidadGenHombre, cantidadGenOtros, costoAlquiler, totalCosto) {
         this.numero = Presupuestos.VersionPpto++;
         this.cantidadInvitadosTotal;
         this.cantidadInvitadosVeg;
@@ -85,36 +87,71 @@ class Presupuestos {
 }
 
 function guardarPpto() {
-    let VersionesPpto = JSON.parse(localStorage.getItem('VersionesPpto'))
+    let VersionesPpto = JSON.parse(sessionStorage.getItem('VersionesPpto'));
 
-    let nuevaVersionPpto = sessionStorage.getItem('VersionPresupuesto')
-    console.log(nuevaVersionPpto)
+    let nuevaVersionPpto = sessionStorage.getItem('VersionPresupuesto');
+
     if (VersionesPpto === null) {
-        VersionesPpto = []
-        VersionesPpto.push(nuevaVersionPpto);
-        console.log(VersionesPpto)
+        VersionesPpto = [];
     }
-    else {
-        VersionesPpto.push(nuevaVersionPpto);
-    }
-    localStorage.setItem('VersionesPpto', VersionesPpto);
+
+    VersionesPpto.push(nuevaVersionPpto);
+    console.log(VersionesPpto)
+    sessionStorage.setItem('VersionesPpto', JSON.stringify(VersionesPpto));
 }
 
 
-function recuperarLocalStorage() {
-    let VersionesLocalStorage = localStorage.getItem('VersionesPpto')
-    console.log(VersionesLocalStorage)
-    VersionesLocalStorage.forEach(version => {
-        console.log(`Cantidad total de invitados: ${version["cantidadInvitadosTotal"]}
-    Invitados Vegetarianos: ${version['cantidadInvitadosVeg']} 
-    Invitados no vegetarianos  ${version['cantidadInvitadosNoVeg']} 
-    Invitados de género femenino: ${version['cantidadGenMujer']} 
-    Invitados de género masculino: ${version['cantidadGenHombre']} 
-    Invitados de otro género: ${version['cantidadGenOtros']} 
-    Costo de alquiler: ${version['cantidadGenOtros']} 
-    Total costo: ${version['totalCosto']}`);
+// function recuperarSessionStorage() {
+//  let VersionesSessionStorage = JSON.parse(sessionStorage.getItem('VersionesPpto'))
+//console.log(VersionesSessionStorage)
+//VersionesSessionStorage.forEach(versionString  => {
+//  let version = JSON.parse(versionString);
+//console.log(`Cantidad total de invitados: ${version['cantidadInvitadosTotal']}`);
+//console.log(`Cantidad de Invitados Veganos: ${version['cantidadInvitadosVeg']}`);
+//console.log(`Cantidad de Invitados No Veganos: ${version['cantidadInvitadosNoVeg']}`);
+//console.log(`Cantidad de invitados de Genero Femenino: ${version['cantidadGenMujer']}`);
+//console.log(`cantidad de Genero Masculino: ${version['cantidadGenHombre']}`);
+//console.log(`cantidad de otro Genero: ${version['cantidadGenOtros']}`);
+//console.log(`costo del Alquiler: ${version['costoAlquiler']}`);
+//console.log(`Total de Costo: ${version['totalCosto']}`);
+//}); 
 
+function mostrarVersionesPpto() {
+    let VersionesPpto = JSON.parse(sessionStorage.getItem('VersionesPpto'));
+
+    if (VersionesPpto === null || VersionesPpto.length === 0) {
+        // Manejar el caso en que no haya versiones disponibles
+        return;
+    }
+
+    let contenedorVersiones = document.getElementById('div-versiones');
+
+    contenedorVersiones.innerHTML = '';
+
+    VersionesPpto.forEach((versionString, index) => {
+        let version = JSON.parse(versionString);
+
+        // Crear un elemento div para esta versión
+        let versionElement = document.createElement('div');
+        versionElement.classList.add('version');
+
+        // Crear y agregar elementos HTML para mostrar los detalles de la versión
+        versionElement.innerHTML = `
+            <h3>Versión ${index + 1}</h3>
+            <p>Cantidad total de invitados: ${version['cantidadInvitadosTotal']}</p>
+            <p>Cantidad de Invitados Veganos: ${version['cantidadInvitadosVeg']}</p>
+            <p>Cantidad de Invitados No Veganos: ${version['cantidadInvitadosNoVeg']}</p>
+            <p>Cantidad de invitados de Genero Femenino: ${version['cantidadGenMujer']}</p>
+            <p>Cantidad de Genero Masculino: ${version['cantidadGenHombre']}</p>
+            <p>Cantidad de otro Genero: ${version['cantidadGenOtros']}</p>
+            <p>Costo del Alquiler: ${version['costoAlquiler']}</p>
+            <p>Total de Costo: ${version['totalCosto']}</p>
+        `;
+
+        // Agregar la versión al contenedor
+        contenedorVersiones.appendChild(versionElement);
     });
-
 }
+
+
 
